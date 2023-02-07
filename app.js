16b3b87add8040778df528bf67875007c359fff8
vib1910601 <huynhduy260301@gmail.com>
2023-02-07 16:44:26 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 //use router
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use("/api/contacts", contactsRouter);
 app.use(cors());
@@ -13,4 +14,14 @@ app.get("/",(req, res)=>{
     res.json({ message : "Welcome to contact book application."});
 });
 
+app.use((req, res, next ) => {
+    return next (new ApiError(404, "Resource not found"));
+});
+
+
+app.use((err, req, res, next)=>{
+    return res.status(error.statusCode || 500).json({
+        message:  error.message || "Internal Server error",
+    });
+});
 module.exports = app;
